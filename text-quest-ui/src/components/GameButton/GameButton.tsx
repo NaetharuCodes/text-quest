abf85514c85bd0f76dc053a5b6230eaa544fa01e
naetharu <naetharu@gmail.com>
2024-10-25 20:15:53 +0100

    Create game board

--- text-quest-ui/src/components/GameButton/GameButton.tsx
+++ text-quest-ui/src/components/GameButton/GameButton.tsx
@@ -1,16 +1,21 @@
 import styles from "./GameButton.module.css";
 
 interface GameButtonProps {
-  icon: React.ReactSVGElement;
+  icon: React.ReactElement<SVGAElement>;
   text?: string;
   onClick: () => void;
 }
 
 const GameButton = ({ icon, text, onClick }: GameButtonProps) => {
   return (
-    <button type="button" title="game-button" onClick={onClick}>
-      {icon}
-      {text && <div>{text}</div>}
+    <button
+      className={styles.button}
+      type="button"
+      title="game-button"
+      onClick={onClick}
+    >
+      <div className={styles.iconContainer}>{icon}</div>
+      {text && <div className={styles.text}>{text}</div>}
     </button>
   );
 };
--- text-quest-ui/src/components/GameControls/GameControls.tsx
+++ text-quest-ui/src/components/GameControls/GameControls.tsx
@@ -1,35 +1,19 @@
-import StandardButton from "../StandardButton/StandardButton";
+import { SunIcon } from "@/icons/SunIcon";
+import GameButton from "../GameButton/GameButton";
 import styles from "./GameControls.module.css";
+import { MoonIcon } from "@/icons/MoonIcon";
+import SparkIcon from "@/icons/SparkIcon";
+import RuneIcon from "@/icons/RuneIcon";
 
 interface GameControlsProps {}
 
 const GameControls = () => {
   return (
     <div className={styles.container}>
-      <StandardButton
-        type="button"
-        btnType="primary"
-        text="Option 1"
-        onClick={() => {}}
-      />
-      <StandardButton
-        type="button"
-        btnType="primary"
-        text="Option 1"
-        onClick={() => {}}
-      />
-      <StandardButton
-        type="button"
-        btnType="primary"
-        text="Option 1"
-        onClick={() => {}}
-      />
-      <StandardButton
-        type="button"
-        btnType="primary"
-        text="Option 1"
-        onClick={() => {}}
-      />
+      <GameButton icon={SunIcon} onClick={() => {}} text="Option 1" />
+      <GameButton icon={MoonIcon} onClick={() => {}} text="Option 2" />
+      <GameButton icon={SparkIcon} onClick={() => {}} text="Option 3" />
+      <GameButton icon={RuneIcon} onClick={() => {}} text="Option 4" />
     </div>
   );
 };
